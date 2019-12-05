48e8ba0a4c74779ddc0d3408d9a6abdf5b297aff
k1-k0 <kmssrvk@gmail.com>
2019-12-06 00:37:00 +0300

    Implemented a primitive worked solution

--- picker.js
+++ picker.js
@@ -2,16 +2,53 @@ const {google} = require('googleapis');
 
 const youtube = google.youtube({
     version: 'v3',
-    auth: 'INSERT_HERE_YOUR_API_KEY'
+    auth: 'INSERT_HERE_YOUR_YOUTUBE_API_KEY'
 });
 
-youtube.commentThreads.list({
-    'part': 'snippet',
-    'videoId': 'INSERT_VIDEO_ID_HERE'
-}).then( response => {
-    console.log(response.data.items.length);
-    for(const comment of response.data.items)
-        console.log(comment.snippet.topLevelComment);
-});
+function getPersonInfo(item) {
+    const comment = item.snippet.topLevelComment.snippet;
+    return {
+        id:     comment.authorChannelUrl,
+        name:   comment.authorDisplayName,
+        avatar: comment.authorProfileImageUrl,
+        text:   comment.textDisplay
+    }
+}
 
-// TODO: Returned only 20 commentaries
+function processResponse(response, action) {
+    return response.data.items.map(action);
+}
+
+async function getComments(videoId) {
+    const startTime = Date.now();
+
+    let comments = [];
+
+    const options = {
+        'part': 'snippet',
+        'videoId': videoId,
+        'maxResults': 100
+    }
+
+    let nextPageToken;
+    const response = await youtube.commentThreads.list(options);
+    
+    nextPageToken = response.data.nextPageToken;
+    comments = comments.concat(processResponse(response, getPersonInfo));
+
+    while (nextPageToken) {
+        options.pageToken = nextPageToken;
+        
+        console.log('Count: ', comments.length);
+
+        const response = await youtube.commentThreads.list(options);
+        comments = comments.concat(processResponse(response, getPersonInfo));
+        nextPageToken = response.data.nextPageToken;
+    }
+
+    console.log(comments.length);
+    const totalTime = Date.now() - startTime;
+    console.log(`Time spent: ${totalTime}ms`)
+}
+
+getComments('INSERT_HERE_VIDEO_ID');
